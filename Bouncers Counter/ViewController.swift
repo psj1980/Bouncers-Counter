eb30c1099bb62b81816d2dec3ee659a14f8593b8
Pia Søvsø Jensen <pia@evilbob.dk>
2015-07-11 16:10:06 +0200

    Initial Commit

---
+++ Bouncers Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bouncers Counter
+//
+//  Created by Pia Jensen on 11/07/15.
+//  Copyright © 2015 Pia Jensen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
